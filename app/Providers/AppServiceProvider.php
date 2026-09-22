53f27c767d7df7cefcdaea0c23a46f1f80ebe3e7
ThekeySea <2azhnanprib@gmail.com>
2026-09-22 14:43:35 +0700

    Vercel deployment: add PHP runtime, SSL cert handler, build config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,16 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        $sslCa = config('database.connections.mysql.options.' . \PDO::MYSQL_ATTR_SSL_CA);
+
+        if ($sslCa && !file_exists($sslCa)) {
+            $caPath = '/tmp/tidb-ca.pem';
+            if (!file_exists($caPath)) {
+                file_put_contents($caPath, $sslCa);
+            }
+            config(['database.connections.mysql.options' => [
+                \PDO::MYSQL_ATTR_SSL_CA => $caPath,
+            ]]);
+        }
     }
 }
